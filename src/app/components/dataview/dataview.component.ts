63d60ee32849f40358ded6d2a1faea32e14535b4
Fernando Chicaiza <washopilot@yahoo.com>
2021-06-13 12:31:49 -0500

    implement LazyLoad

--- src/app/components/dataview/dataview.component.ts
+++ src/app/components/dataview/dataview.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { APIResponse, Result } from 'src/app/models';
 import { HttpService } from 'src/app/services/http.service';
+import { LazyLoadEvent } from 'primeng/api';
 
 @Component({
   selector: 'app-dataview',
@@ -9,14 +10,31 @@ import { HttpService } from 'src/app/services/http.service';
 })
 export class DataviewComponent implements OnInit {
   games: Result[] = [];
+  totalRecords: number = 0;
+  loading: boolean = true;
+  first: number = 0;
+  rows: number = 0;
 
   constructor(private httpService: HttpService) {}
 
-  ngOnInit(): void {
-    this.httpService.getGameList().subscribe(
+  ngOnInit(): void {}
+
+  loadData(event: LazyLoadEvent) {
+    this.first = event.first || 0;
+    this.rows = event.rows || 1;
+    this.getList(this.first / this.rows + 1, this.rows);
+  }
+
+  getList(page?: number, page_size?: number) {
+    this.loading = true;
+    this.httpService.getGameData(page, page_size).subscribe(
       (gameList: APIResponse) => {
         this.games = gameList.results;
+        this.totalRecords = gameList.count;
+        this.loading = false;
         console.log(this.games);
+        console.log(gameList);
+        console.log(this.first, this.rows, this.totalRecords);
       },
       (error) => console.log(error)
     );
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -9,17 +9,35 @@ import { APIResponse } from '../models';
 export class HttpService {
   baseURL: string = 'https://api.rawg.io/api';
 
-  params = new HttpParams()
-    .set('key', 'c52d1f197c8a48919f54b886a736ba47')
-    .set('page', 1)
-    .set('page_size', 40);
-  // .set('ordering', 'released');
-
   constructor(private http: HttpClient) {}
 
-  getGameList(): Observable<APIResponse> {
+  getGameData(page?: number, page_size?: number): Observable<APIResponse> {
+    let params = new HttpParams().set(
+      'key',
+      'c52d1f197c8a48919f54b886a736ba47'
+    );
+
+    if (page) {
+      params = new HttpParams()
+        .set('key', 'c52d1f197c8a48919f54b886a736ba47')
+        .set('page', page);
+    }
+
+    if (page_size) {
+      params = new HttpParams()
+        .set('key', 'c52d1f197c8a48919f54b886a736ba47')
+        .set('page_size', page_size);
+    }
+
+    if (page && page_size) {
+      params = new HttpParams()
+        .set('key', 'c52d1f197c8a48919f54b886a736ba47')
+        .set('page', page)
+        .set('page_size', page_size);
+    }
+
     return this.http.get<APIResponse>(this.baseURL + '/games', {
-      params: this.params,
+      params: params,
     });
   }
 }
